f492b6e988e5a0d0458732d778f4e12130f2e60a
heyitsGaspar <israel.chay.32@gmail.com>
2025-07-09 16:17:38 -0600

    feat: adding methods to repositories

--- src/modules/users/domain/repositories/userRepository.ts
+++ src/modules/users/domain/repositories/userRepository.ts
@@ -3,4 +3,9 @@ import { UserEntity } from "../entities/userEntity";
 export interface UserRepositoryI {
     create(user: UserEntity): Promise<UserEntity>;
     findByEmail(email: string): Promise<UserEntity | null>;
+    getAll(skip: number, take: number): Promise<UserEntity[]>;
+    findById(id: string): Promise<UserEntity | null>;
+    update(id: string, user: Partial<UserEntity>): Promise<UserEntity | null>;
+    delete(id: string): Promise<void>;
+    count(): Promise<number>;
 }
--- src/modules/users/infrastructure/repositories/userRepositoryPrisma.ts
+++ src/modules/users/infrastructure/repositories/userRepositoryPrisma.ts
@@ -27,5 +27,52 @@ export const userRepository: UserRepositoryI = {
             where: { email },
             
         });
-    }
+    },
+
+    async getAll(skip: number, take: number): Promise<UserEntity[]> {
+        return prisma.users.findMany({
+            skip,
+            take,
+            include: {
+                role: true // Include the role in the response
+            }
+        });
+    },
+
+    async findById(id: string): Promise<UserEntity | null> {
+        return prisma.users.findUnique({
+            where: { id },
+            include: {
+                role: true // Include the role in the response
+            }
+        });
+    },
+
+    async update(id: string, user: Partial<UserEntity>): Promise<UserEntity | null> {
+        return prisma.users.update({
+            where: { id },
+            data: {
+                name: user.name,
+                email: user.email,
+                password: user.password,
+                role: {
+                    connect: { id: user.roleId }
+                }
+            },
+            include: {
+                role: true // Include the role in the response
+            }
+        });
+    },
+
+    async delete(id: string): Promise<void> {
+        await prisma.users.delete({
+            where: { id }
+        });
+    },
+
+    async count(): Promise<number> {
+        return prisma.users.count();
+    },
+    
 }
